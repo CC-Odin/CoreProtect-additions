7062c85affdf710a95eee943003635ce2f5a7f63
Alberto Gusmeroli <96133902+guss-alberto@users.noreply.github.com>
2026-02-01 18:30:54 +0100

    remove unused import

--- src/main/java/com/alisaa/coreprotectadditions/BedLogger.java
+++ src/main/java/com/alisaa/coreprotectadditions/BedLogger.java
@@ -8,7 +8,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerBedEnterEvent.BedEnterResult;
 
 import io.papermc.paper.block.bed.*;
 
